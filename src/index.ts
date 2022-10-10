74f877bd819da6ccef0fa6931aded62d28751c9d
Trey <trey200654@gmail.com>
2022-10-10 12:53:52 -0400

    V1

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,45 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
 
-import confetti from 'canvas-confetti';
+const form = <HTMLFormElement> document.getElementById('new-task-form')
+const input = <HTMLInputElement> document.getElementById('new-task-title')
+const todo = document.createElement('input')
+const editbtn = document.createElement('button')
+const delbtn = document.createElement('button')
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+form.addEventListener('submit',e => {
+	e.preventDefault();
+	const todo = document.createElement('input')
+	todo.id = 'new-todo'
+	todo.readOnly = true
+	todo.value = input.value
+	todo.type = 'text'
+	input.value = ''
+	const editbtn = document.createElement('button')
+	editbtn.id = 'edit'
+	editbtn.innerHTML = 'Edit'
+	const delbtn = document.createElement('button')
+	delbtn.innerHTML = 'Delete'
+	delbtn.id = 'del'
+	const container = document.createElement('div')
+	container.id = 'todo-div'
+	container.append(todo)
+	container.append(editbtn)
+	container.append(delbtn)
+	const todoContainer = <HTMLDivElement> document.getElementById('todo-cont')
+	todoContainer.append(container)
+
+	delbtn.addEventListener('click',() => {
+		delbtn?.parentElement?.remove()
+	})
+
+	editbtn.addEventListener('click',() => {
+		if (editbtn.innerHTML === 'Save') {
+			todo.readOnly = true
+			editbtn.innerHTML = 'Edit'
+		} else {
+			todo.readOnly = false
+			editbtn.innerHTML = 'Save'
+		}
+	})
+})
+
+
